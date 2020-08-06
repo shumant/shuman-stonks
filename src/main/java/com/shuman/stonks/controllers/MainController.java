a96461ec9f02742c6007aeb3d2a0f4292ad66aab
anton.shumskih <anton.shumskih@revolut.com>
2020-08-06 10:04:24 +0300

    stream

--- src/main/java/com/shuman/stonks/controllers/MainController.java
+++ src/main/java/com/shuman/stonks/controllers/MainController.java
@@ -1,21 +1,21 @@
 package com.shuman.stonks.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ResolvableType;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.annotation.PostConstruct;
 import java.util.Collection;
@@ -25,13 +25,17 @@ import java.util.Optional;
 
 @Controller
 public class MainController {
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
-    @Autowired
-    private OAuth2AuthorizedClientService clientService;
+    private final ClientRegistrationRepository clientRegistrationRepository;
+    private final OAuth2AuthorizedClientService clientService;
 
     private Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+
+    @Autowired
+    public MainController(ClientRegistrationRepository clientRegistrationRepository,
+                          OAuth2AuthorizedClientService clientService) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+        this.clientService = clientService;
+    }
 
     @PostConstruct
     public void init() {
@@ -39,6 +43,12 @@ public class MainController {
             .map(registration ->
             oauth2AuthenticationUrls.put(registration.getClientName(),
                 "oauth2/authorization/" + registration.getRegistrationId()));
+    }
+
+    @PostMapping(path = "/stream", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public String stream(Model model, @RequestBody MultiValueMap<String, String> formData) {
+        model.addAttribute("streamer", formData.getFirst("streamer"));
+        return "stream";
     }
 
     @GetMapping("/")
